499e1bb1e8b6be018c0944d4d1865932545a6062
sodamjeong <sodam828@naver.com>
2023-03-22 13:38:51 +0900

    modify

--- study/DJANGO/practice/django1/articles/views.py
+++ study/DJANGO/practice/django1/articles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-# 특정 기능을 수행하는 viw 함수를 만듦
+# 특정 기능을 수행하는 view 함수를 만듦
 
 def index(request):
     return render(request, 'articles/index.html')
